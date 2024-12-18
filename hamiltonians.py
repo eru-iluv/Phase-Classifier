1c7c198cf700f11246a8fd8b2483425f4639f9de
eru-iluv <edgardmacena@usp.br>
2024-12-17 23:41:31 -0300

    inicia arquivos hamiltonians.py e utils.py

---
+++ hamiltonians.py
@@ -0,0 +1,159 @@
+import utils;
+import numpy as np
+from scipy.sparse import csr_array, kron, eye, linalg
+
+class Hamiltonian:
+    def __init__(self, n, spin='1'):
+        """
+        Parameters
+        ----------
+        n : int
+            Number of spins in the system
+        spin: str
+            Spin value of the system
+        """
+
+        self._n = n
+        self._spin = spin
+        self._matrix =csr_array((utils.spin_states[spin]**self._n, 
+            utils.spin_states[spin]**self._n), dtype=np.complex64)
+        self._kroned_identities = [ 
+                eye(utils.spin_states[spin]**i) for i in range(0,n-1)
+            ]
+    @property
+    def n(self) -> int:
+        return self._n
+    @property
+    def spin(self) -> str:
+        return self._spin
+    @property
+    def matrix_dim(self) -> int:
+        return self._matrix.shape[0]
+    
+    def kroned_identity(self, index) -> np.array:
+        
+        return self._kroned_identities[index]
+
+    def _build_term(self, i, operator, coeff):
+        return kron(
+        kron(self.kroned_identity(i), operator),
+        kron(operator, self.kroned_identity(self._n - 2 - i))
+    ) * coeff
+
+    def _cyclical_term(self, operator, coeff):
+        return kron(
+            operator, kron(
+            self.kroned_identity(self._n - 2),
+            operator)
+        ) * coeff
+
+    def __str__(self):
+        return f"Hamiltonian of {self.n} spins of value {self.spin}"
+
+class BondAlternatingXXZ(Hamiltonian):
+    def __init__(self, n, Delta, delta, spin='1'):
+        """
+        Parameters
+        ----------
+        n : int
+            Number of spins in the system
+        Delta : float
+            Anisotropy parameter
+        delta : float
+            Alternation parameter
+        spin: str
+            Spin value of the system.
+        """
+        super().__init__(n, spin)
+        self._Delta = Delta 
+        self._delta = delta
+
+        # Build the Hamiltonian matrix for the non-cyclic terms
+        for l in range(n-1):
+            coeff = 1 - delta * (-1)**(l+1)
+            self._matrix += self._build_term(l, utils.spin_operators[spin]['Sx'], coeff)   # S_l^x S_{l+1}^x term
+            self._matrix += self._build_term(l, utils.spin_operators[spin]['Sy'], coeff)   # S_l^y S_{l+1}^y term
+            self._matrix += self._build_term(l, utils.spin_operators[spin]['Sz'], Delta * coeff)   # S_l^z S_{l+1}^z term
+
+        # Cyclical terms
+        coeff = 1 - delta * (-1)**n
+        self._matrix += self._cyclical_term(utils.spin_operators[spin]['Sx'], coeff)  # S_N^x S_1^x term
+        self._matrix += self._cyclical_term(utils.spin_operators[spin]['Sy'], coeff)  # S_N^y S_1^y term  
+        self._matrix += self._cyclical_term(utils.spin_operators[spin]['Sz'], Delta * coeff) # S_N^z S_1^z term
+
+        self._matrix = csr_array(self._matrix)
+    
+    @property
+    def Delta(self):
+        return self._Delta
+    @property
+    def delta(self):
+        return self._Delta
+    
+    def __str__(self):
+        return super().__str__()  +  \
+        f"""\nBond-alternating XXZ chain\n\nHamiltonian properties:
+        δ = {self.delta} 
+        Δ = {self.Delta}.\n"""
+        
+
+class XXZUniaxialSingleIonAnisotropy(Hamiltonian):
+    def __init__(self, n, Jz, D, spin='1', J=1):
+        """
+        Parameters
+        ----------
+        n : int
+            Number of spins in the system
+        J : float
+            Spin coupling parameter in the xy-axis
+        Jz : float
+            Spin coupling parameter in the z-axis
+        D : float
+            Uniaxial single ion anisotropy parameter
+        spin: str
+            Spin value of the system.
+        """
+        super().__init__(n, spin)
+        self._Jz = Jz 
+        self._D = D
+
+        # Build the Hamiltonian matrix for the non-cyclic terms
+        for l in range(n-1):
+            self._matrix += self._build_term(l, utils.spin_operators[spin]['Sx'], J)   # S_l^x S_{l+1}^x term
+            self._matrix += self._build_term(l, utils.spin_operators[spin]['Sy'], J)   # S_l^y S_{l+1}^y term
+            self._matrix += self._build_term(l, utils.spin_operators[spin]['Sz'], Jz)   # S_l^z S_{l+1}^z term
+            self._matrix += self._build_anisotropy(l, utils.spin_operators[spin]['Sz2'], D)   # S_l^z2 term
+        # Cyclical terms
+        self._matrix += self._cyclical_term(utils.spin_operators[spin]['Sx'], J)  # S_N^x S_1^x term
+        self._matrix += self._cyclical_term(utils.spin_operators[spin]['Sy'], J)  # S_N^y S_1^y term  
+        self._matrix += self._cyclical_term(utils.spin_operators[spin]['Sz'], Jz) # S_N^z S_1^z term
+        self._matrix += self._build_anisotropy(n-1, utils.spin_operators[spin]['Sz2'], D)   # S_l^z2 term
+
+        self._matrix = csr_array(self._matrix)
+    
+    def _build_anisotropy(self, i, operator, coeff):
+        return kron(
+        kron(self.kroned_identity(i), operator),
+        self.kroned_identity(self._n - 1 - i)
+    ) * coeff
+    
+    @property
+    def Delta(self):
+        return self._Delta
+    @property
+    def delta(self):
+        return self._Delta
+    
+    def __str__(self):
+        return super().__str__()  +  \
+        f"""XXZ chains with uniaxial single-ion-type anisotropy,\n\nHamiltonian properties:
+        Jz = {self.Jz} 
+        D = {self.D}.\n"""
+
+
+'''eigvalues, eigvec =  linalg.eigs(x._matrix, k=1)
+
+eigvalue = eigvalues[0],
+eigvec = eigvec[:,0]
+
+print(eigvalue, eigvec)'''
---
+++ utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+
+spin_states = { 
+    '1': 3,
+    '1/2': 2,
+}
+
+spin_operators = {
+    '1': {
+        'Sz' : csr_matrix( np.array([[1,0,0],[0,0,0],[0,0,-1]])),
+        'Sz2' : csr_matrix(np.array([[1,0,0],[0,0,0],[0,0,1]])),
+        'Sx' : csr_matrix(1/np.sqrt(2)*np.array([[0,1,0],[1,0,1],[0,1,0]])),
+        'Sy' : csr_matrix(1j/np.sqrt(2)*np.array([[0,-1,0],[1,0,-1],[0,1,0]])),
+    },
+
+    '1/2': {
+        'Sz' : np.array([[1,0],[0,-1]])/2,
+        'Sx' : np.array([[0,1],[1,0]])/2,
+        'Sy' : np.array([[0,-1j],[1j,0]])/2,
+    }
+}
