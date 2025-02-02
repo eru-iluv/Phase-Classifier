a23b09e1cfcb8e75fb93d043a03971b68f525eb1
eru-iluv <edgardmacena@usp.br>
2025-02-01 23:47:58 -0300

    ADD: geração de dados e apresentação ML

--- data_generation.py
+++ data_generation.py
@@ -4,7 +4,7 @@ import correlators as c
 from timeit import default_timer as timer
 
 
-def generate_data(N):
+def generate_data(N, to_generate = ['H1', 'H2', 'H3']):
 
     H_1_parameters = np.arange(-4, 4, 0.1)
     Jzs, Ds = H_1_parameters, H_1_parameters
@@ -25,79 +25,81 @@ def generate_data(N):
     corr = c.Correlators(N)
     n_sup = int(N/2+1)
 
+    if 'H1' in to_generate:
+        with open(f"data/H1/N={N}.csv", 'w') as fh:
+            fh.write("Jz, D, " + ", ".join([f"S1S{i}{j}" for i in range(1, n_sup+1) for j in ['x', 'y', 'z']]) + ", "+
+            ", ".join([f"prodSi{j}" for j in ['x', 'y', 'z']]))
 
-    with open(f"data/H1/N={N}.csv", 'w') as fh:
-        fh.write("Jz, D, " + ", ".join([f"S1S{i}{j}" for i in range(1, n_sup+1) for j in ['x', 'y', 'z']]) + ", "+
-        ", ".join([f"prodSi{j}" for j in ['x', 'y', 'z']]))
+            for Jz in Jzs:
+                for D in Ds:
+                    line = [Jz, D]
+                    try: 
+                        H1 = h.XXZUniaxialSingleIonAnisotropy(N, Jz, D)
+                        gstate = H1.gstate
+                    except: 
+                        continue  
+                    gstate_dagg = np.conj(gstate).T
+                    for i in range(n_sup):
+                        line.append(np.real(gstate_dagg @ corr.S1Six(i) @ gstate))
+                        line.append(np.real(gstate_dagg @ corr.S1Siy(i) @ gstate))
+                        line.append(np.real(gstate_dagg @ corr.S1Siz(i) @ gstate))
+                    
+                    line.append(np.real(gstate_dagg @ corr.prodSix @ gstate))
+                    line.append(np.real(gstate_dagg @ corr.prodSiy @ gstate))
+                    line.append(np.real(gstate_dagg @ corr.prodSiz @ gstate))
 
-        for Jz in Jzs:
-            for D in Ds:
-                line = [Jz, D]
-                try: 
-                    H1 = h.XXZUniaxialSingleIonAnisotropy(N, Jz, D)
-                    gstate = H1.gstate
-                except: 
-                    continue  
-                gstate_dagg = np.conj(gstate).T
-                for i in range(n_sup):
-                    line.append(np.real(gstate_dagg @ corr.S1Six(i) @ gstate))
-                    line.append(np.real(gstate_dagg @ corr.S1Siy(i) @ gstate))
-                    line.append(np.real(gstate_dagg @ corr.S1Siz(i) @ gstate))
-                
-                line.append(np.real(gstate_dagg @ corr.prodSix @ gstate))
-                line.append(np.real(gstate_dagg @ corr.prodSiy @ gstate))
-                line.append(np.real(gstate_dagg @ corr.prodSiz @ gstate))
+                    fh.write("\n"+", ".join([str(i) for i in line]))
 
-                fh.write("\n"+", ".join([str(i) for i in line]))
-
-    with open(f"data/H2/N={N}.csv", 'w') as fh:
-        fh.write("Delta, delta, " + ", ".join([f"S1S{i}{j}" for i in range(1, n_sup+1) for j in ['x', 'y', 'z']]) + ", "+
-        ", ".join([f"prodSi{j}" for j in ['x', 'y', 'z']]))
+    if 'H2' in to_generate:
+        with open(f"data/H2/N={N}.csv", 'w') as fh:
+            fh.write("Delta, delta, " + ", ".join([f"S1S{i}{j}" for i in range(1, n_sup+1) for j in ['x', 'y', 'z']]) + ", "+
+            ", ".join([f"prodSi{j}" for j in ['x', 'y', 'z']]))
 
 
-        for Delta in Deltas:
-            for delta in deltas:
-                line = [Delta, delta]
+            for Delta in Deltas:
+                for delta in deltas:
+                    line = [Delta, delta]
+                    try:
+                        H2 = h.BondAlternatingXXZ(N, Delta, delta)
+                        gstate = H2.gstate
+                    except:
+                        continue 
+                    gstate_dagg = np.conj(gstate).T
+                    for i in range(n_sup):
+                        line.append(np.real(gstate_dagg @ corr.S1Six(i) @ gstate))
+                        line.append(np.real(gstate_dagg @ corr.S1Siy(i) @ gstate))
+                        line.append(np.real(gstate_dagg @ corr.S1Siz(i) @ gstate))
+                    
+                    line.append(np.real(gstate_dagg @ corr.prodSix @ gstate))
+                    line.append(np.real(gstate_dagg @ corr.prodSiy @ gstate))
+                    line.append(np.real(gstate_dagg @ corr.prodSiz @ gstate))
+
+                    fh.write("\n"+", ".join([str(i) for i in line]))
+
+    if 'H3' in to_generate:
+        with open(f"data/H3/N={N}.csv", 'w') as fh:
+            fh.write("theta, -1, " + ", ".join([f"S1S{i}{j}" for i in range(1, n_sup+1) for j in ['x', 'y', 'z']]) + ", "+
+            ", ".join([f"prodSi{j}" for j in ['x', 'y', 'z']]))
+
+
+            for theta in thetas:
+                line = [theta, -1]
                 try:
-                    H2 = h.BondAlternatingXXZ(N, Delta, delta)
-                    gstate = H2.gstate
+                    H3 = h.BilinearBiquadratic(N, theta)
+                    gstate = H3.gstate
                 except:
-                    continue 
+                    continue
                 gstate_dagg = np.conj(gstate).T
                 for i in range(n_sup):
                     line.append(np.real(gstate_dagg @ corr.S1Six(i) @ gstate))
                     line.append(np.real(gstate_dagg @ corr.S1Siy(i) @ gstate))
                     line.append(np.real(gstate_dagg @ corr.S1Siz(i) @ gstate))
-                
+                    
                 line.append(np.real(gstate_dagg @ corr.prodSix @ gstate))
                 line.append(np.real(gstate_dagg @ corr.prodSiy @ gstate))
                 line.append(np.real(gstate_dagg @ corr.prodSiz @ gstate))
-
+            
                 fh.write("\n"+", ".join([str(i) for i in line]))
-
-    with open(f"data/H3/N={N}.csv", 'w') as fh:
-        fh.write("theta, -1, " + ", ".join([f"S1S{i}{j}" for i in range(1, n_sup+1) for j in ['x', 'y', 'z']]) + ", "+
-        ", ".join([f"prodSi{j}" for j in ['x', 'y', 'z']]))
-
-
-        for theta in thetas:
-            line = [theta, -1]
-            try:
-                H3 = h.BilinearBiquadratic(N, theta)
-                gstate = H3.gstate
-            except:
-                continue
-            gstate_dagg = np.conj(gstate).T
-            for i in range(n_sup):
-                line.append(np.real(gstate_dagg @ corr.S1Six(i) @ gstate))
-                line.append(np.real(gstate_dagg @ corr.S1Siy(i) @ gstate))
-                line.append(np.real(gstate_dagg @ corr.S1Siz(i) @ gstate))
-                
-            line.append(np.real(gstate_dagg @ corr.prodSix @ gstate))
-            line.append(np.real(gstate_dagg @ corr.prodSiy @ gstate))
-            line.append(np.real(gstate_dagg @ corr.prodSiz @ gstate))
-        
-            fh.write("\n"+", ".join([str(i) for i in line]))
 
 
 start = timer()
